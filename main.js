4ca51de945738903c60681d3caa9ef9237b95448
David Kuehn <dmkuehn6@gmail.com>
2019-01-29 10:25:10 -0600

    Commented main.js

--- main.js
+++ main.js
@@ -7,12 +7,16 @@ const path = require('path');
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 
+//Pull routes from llapi.js (all LLAPI routes)
 const llapi = require('./llapi');
 app.use('/llapi', llapi);
 
+//Make public files accessible
 app.use(express.static('public'));
 
+//Route: /newsite
 app.get('/newsite', function (req, res) {
+  //Serve static HTML page
   res.sendFile(path.join(__dirname, 'public', 'newsite.html'));
 });
 
